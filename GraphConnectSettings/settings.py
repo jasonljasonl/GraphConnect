a61d491b916204902d0164ce94d35fba6c49f043
jasonljasonl <jasonlandim@outlook.fr>
2025-03-29 03:25:11 +0100

    updating settings.py

--- GraphConnectSettings/settings.py
+++ GraphConnectSettings/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 REST_FRAMEWORK = {
@@ -73,14 +74,13 @@ SIMPLE_JWT = {
 }
 
 
-#CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000',
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://graphconnect-frontend-695590394372.europe-west1.run.app",
+    "https://graphconnect-frontend-695590394372.europe-west1.run.app",  # Your frontend URL
 ]
 
 
