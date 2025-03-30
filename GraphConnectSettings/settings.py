62f6ac2787d29c596cd03921ab917ec7a36f6ba7
jasonljasonl <jasonlandim@outlook.fr>
2025-03-30 15:06:26 +0200

    updating database_host

--- GraphConnectSettings/settings.py
+++ GraphConnectSettings/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
+        'HOST': '/cloudsql/GraphConnect:europe-west1:graphconnect-db',
         'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
