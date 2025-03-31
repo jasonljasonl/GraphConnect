4187a7ffa2b9b7abe5f25745d698b39e0241db8d
jasonljasonl <jasonlandim@outlook.fr>
2025-03-31 02:42:47 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,44 @@
 #!/bin/sh
 
-# Start Cloud SQL Proxy with the IP address of Cloud SQL
-./cloud_sql_proxy -dir=/cloudsql -ip_address=34.79.74.37 &
+# Vérifiez si le fichier cloud_sql_proxy existe
+if [ ! -f "./cloud_sql_proxy" ]; then
+  echo "Le fichier cloud_sql_proxy n'existe pas. Téléchargement..."
+  # Télécharger cloud_sql_proxy si le fichier n'existe pas
+  curl -o /cloud_sql_proxy https://storage.googleapis.com/cloudsql-proxy/v1.33.4/cloud_sql_proxy.linux.amd64 && \
+    chmod +x /cloud_sql_proxy
+fi
 
-# Get the PID of Cloud SQL Proxy to manage it later
+# Démarrer le Cloud SQL Proxy avec l'adresse IP de Cloud SQL
+if [ -z "$CLOUD_SQL_INSTANCE" ]; then
+  # Utilisation de l'adresse IP si le socket est indisponible
+  ./cloud_sql_proxy -dir=/cloudsql -ip_address=34.79.74.37 &
+else
+  # Utilisation du socket Cloud SQL si dans GCP
+  ./cloud_sql_proxy -dir=/cloudsql &
+fi
+
+# Récupérer le PID du Cloud SQL Proxy pour le gérer ensuite
 PROXY_PID=$!
 
-# Wait for Cloud SQL Proxy to be ready
+# Attendre que le Cloud SQL Proxy soit prêt
 echo "Waiting for Cloud SQL Proxy to start..."
-sleep 5  # Gives the proxy time to establish the connection
+sleep 5  # Laisser du temps pour établir la connexion
 
-# Wait for the database to be ready via TCP
+# Attendre que la base de données soit prête via TCP
 echo "Waiting for the database..."
 while ! nc -z 34.79.74.37 5432; do
   sleep 1
 done
 echo "Database is ready."
 
-# Apply migrations
+# Appliquer les migrations
 echo "Applying migrations..."
 python manage.py migrate --noinput
 
-# Collect static files
+# Collecter les fichiers statiques
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Start Gunicorn to serve the application
+# Démarrer Gunicorn pour servir l'application
 echo "Starting Gunicorn..."
 exec gunicorn GraphConnectSettings.wsgi:application --bind 0.0.0.0:8080
