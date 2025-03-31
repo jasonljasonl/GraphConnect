5dfb9eb11e139a61c11ec59297691ad6cec62e81
jasonljasonl <jasonlandim@outlook.fr>
2025-03-31 09:11:17 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,20 @@
 #!/bin/sh
 
-# Vérifiez si le fichier cloud_sql_proxy existe
-if [ ! -f "./cloud_sql_proxy" ]; then
+# Vérification si Cloud SQL Proxy est déjà présent
+if [ ! -f "/usr/local/bin/cloud_sql_proxy" ]; then
   echo "Le fichier cloud_sql_proxy n'existe pas. Téléchargement..."
-  # Télécharger cloud_sql_proxy si le fichier n'existe pas
-  curl -o /cloud_sql_proxy https://storage.googleapis.com/cloudsql-proxy/v1.33.4/cloud_sql_proxy.linux.amd64 && \
-    chmod +x /cloud_sql_proxy
+  curl -o /cloud_sql_proxy https://storage.googleapis.com/cloudsql-proxy/v1.33.4/cloud_sql_proxy.linux.amd64
+  chmod +x /cloud_sql_proxy
+  mv /cloud_sql_proxy /usr/local/bin/cloud_sql_proxy
 fi
 
-# Démarrer le Cloud SQL Proxy avec l'adresse IP de Cloud SQL
-if [ -z "$CLOUD_SQL_INSTANCE" ]; then
-  # Utilisation de l'adresse IP si le socket est indisponible
-  ./cloud_sql_proxy -dir=/cloudsql -ip_address=34.79.74.37 &
-else
-  # Utilisation du socket Cloud SQL si dans GCP
-  ./cloud_sql_proxy -dir=/cloudsql &
-fi
+# Vérifier si le fichier est bien exécutable
+ls -lah /usr/local/bin/cloud_sql_proxy
 
-# Récupérer le PID du Cloud SQL Proxy pour le gérer ensuite
-PROXY_PID=$!
+# Lancer le proxy Cloud SQL
+cloud_sql_proxy -dir=/cloudsql -ip_address=34.79.74.37 &
 
-# Attendre que le Cloud SQL Proxy soit prêt
-echo "Waiting for Cloud SQL Proxy to start..."
-sleep 5  # Laisser du temps pour établir la connexion
-
-# Attendre que la base de données soit prête via TCP
+# Attendre que la base de données soit prête
 echo "Waiting for the database..."
 while ! nc -z 34.79.74.37 5432; do
   sleep 1
