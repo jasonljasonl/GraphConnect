b08404e6fbf0f8c5587cfaf3b24348f2a7cc38ea
jasonljasonl <jasonlandim@outlook.fr>
2025-04-02 11:23:20 +0200

    removing secret google credentials

--- GraphConnectSettings/settings.py
+++ GraphConnectSettings/settings.py
@@ -206,6 +206,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.CustomUser'
 
+google_credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+if google_credentials_path:
+    print(f"✅ Chemin des credentials trouvé : {google_credentials_path}")
+else:
+    print("❌ Erreur : GOOGLE_APPLICATION_CREDENTIALS non défini")
+
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_images')
 #MEDIA_URL = 'https://storage.googleapis.com/graph-connect_bucket/'
 
