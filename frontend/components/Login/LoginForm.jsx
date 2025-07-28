0b7fbcddec6a81687ca8b944a825aa2d7b797b75
jasonljasonl <81412142+jasonljasonl@users.noreply.github.com>
2025-07-28 16:17:13 +0200

    Update LoginForm.jsx

--- frontend/components/Login/LoginForm.jsx
+++ frontend/components/Login/LoginForm.jsx
@@ -69,13 +69,7 @@ const LoginForm = () => {
                 <label htmlFor="password" className="block text-sm/6 font-medium ">
                   Password
                 </label>
-                {/*
-                    <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
-                */}
+
               </div>
               <div className="mt-2">
                 <input
