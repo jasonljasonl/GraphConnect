4dd4e25b256c6804610500456828d7d9592f106d
jasonljasonl <81412142+jasonljasonl@users.noreply.github.com>
2025-07-28 16:13:20 +0200

    Update Homepage.jsx

--- frontend/components/Homepage.jsx
+++ frontend/components/Homepage.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import PostsListTemplate from './Templates/PostsListTemplate.jsx'
 
 export default function Homepage() {
-//    const API_BASE_URL = 'https://graphconnect-695590394372.europe-west1.run.app/api/';
     const API_BASE_URL = 'http://localhost:8080/api/';
 
     return (
